a7d7dd195cbf7335413d9f3f3e59b5e1b76976d2
mori685 <yasuhiro315014@gmail.com>
2025-02-20 22:06:48 +0900

    Update 3_General_Chat.py

--- pages/3_General_Chat.py
+++ pages/3_General_Chat.py
@@ -1,6 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage
