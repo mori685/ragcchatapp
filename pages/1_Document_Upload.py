97823d11b12d1dd959aeef1b29810a06d59722bb
mori685 <yasuhiro315014@gmail.com>
2025-02-20 22:06:22 +0900

    Update 1_Document_Upload.py

--- pages/1_Document_Upload.py
+++ pages/1_Document_Upload.py
@@ -1,6 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
